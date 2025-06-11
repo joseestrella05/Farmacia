b61f142480d1db1bfc30a23bf817067ba5ee1c53
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-06-11 00:41:54 -0400

    Added a new Medicamentos class in Medicamentos.cs

---
+++ Models/Medicamentos.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Farmacia.Models;
+
+public class Medicamentos
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Es obligatorio introducir un nombre.")]
+    public string? Nombre { get; set; }
+    [Required(ErrorMessage = "Es obligatorio introducir el codigo de barra.")]
+    public string? CodigoBarras { get; set; }
+    public string? PrincipioActivo { get; set; }
+    [Required(ErrorMessage = "Es obligatorio introducir el Lote.")]
+    public string? Lote { get; set; }
+    [Required(ErrorMessage = "Es obligatorio introducir la fecha de caducidad.")]
+    public DateTime FechaCaducidad { get; set; }
+    [Required(ErrorMessage = "Es obligatorio introducir el precio.")]
+    public decimal Precio { get; set; }
+    [Required(ErrorMessage = "Es obligatorio introducir el stock.")]
+    public int Stock { get; set; }
+
+    public bool EsControlado { get; set; } // Medicamento controlado
+}
