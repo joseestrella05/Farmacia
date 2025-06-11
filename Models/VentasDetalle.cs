c3d059c7e38704ab4e5df4b3b8206f469cfb89fe
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-06-11 14:19:41 -0400

    Add VentasDetalle class

---
+++ Models/VentasDetalle.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Farmacia.Models;
+
+public class VentasDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int VentaId { get; set; }
+    public int MedicamentoId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal PrecioUnitario { get; set; }
+    public decimal Descuento { get; set; } 
+    public bool RequiereReceta { get; set; }
+}
