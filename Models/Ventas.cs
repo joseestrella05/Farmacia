0b5f45dde476a2e4d943b4f044328976b9a70c98
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-06-11 14:19:09 -0400

    Add Ventas class

---
+++ Models/Ventas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Farmacia.Models;
+
+public class Ventas
+{
+    public int VentaId { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    public decimal Total { get; set; }
+    public string? TipoPago { get; set; }
+    public string? ClienteId { get; set; }
+    [ForeignKey("ClienteId")]
+    public Clientes? Clientes { get; set; }
+    public List<VentasDetalle> Detalles { get; set; }
+}
